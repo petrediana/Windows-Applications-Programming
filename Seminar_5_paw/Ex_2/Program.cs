30302051bcafdfbaaabbe2b5d662cec6a9c6d8b6
Petre Diana <petre.diana98@yahoo.ro>
2019-03-21 16:16:55 +0200

    seminar 5

---
+++ Seminar_5_paw/Ex_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar_5
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            int n = Citire("Nr elemente: ");
+
+            int[] v = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                v[i] = Citire("element " + (i + 1));
+            }
+
+            MessageBox.Show(v.Sum().ToString());
+
+           
+
+        }
+
+        static int Citire(string mesaj)
+        {
+            Form1 myform = new Form1(mesaj);
+            myform.ShowDialog();
+            return(myform.Rezultat);
+        }
+    }
+}
---
+++ Seminar_5_paw/Ex_3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1(int n)
+        {
+            InitializeComponent();
+
+            int W = Size.Width, H = Size.Height;
+            int w = W / n, h = H / n;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    Panel p = new Panel();
+                    p.Location = new Point(i * w, j * h);
+                    p.BorderStyle = BorderStyle.FixedSingle;
+                    p.Size = new Size(w, h);
+                    Controls.Add(p);
+                }
+            }
+        }
+    }
+}
