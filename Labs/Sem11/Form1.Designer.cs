210957a97501de954003ff916bbfb4458717f330
Petre Diana <petre.diana98@yahoo.ro>
2019-05-09 16:08:59 +0300

    Seminar 11

---
+++ Labs/Sem11/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grafic1 = new WindowsFormsApp1.Grafic();
+            this.SuspendLayout();
+            // 
+            // grafic1
+            // 
+            this.grafic1.Location = new System.Drawing.Point(111, 35);
+            this.grafic1.Name = "grafic1";
+            this.grafic1.Size = new System.Drawing.Size(148, 238);
+            this.grafic1.TabIndex = 0;
+            this.grafic1.Text = "grafic1";
+            this.grafic1.Valori = new int[] {
+        6,
+        10,
+        8,
+        4};
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grafic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Grafic grafic1;
+    }
+}
+
---
+++ Labs/Sem11/Grafic.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace WindowsFormsApp1
+{
+    class Grafic : Control
+    {
+        int[] valori = new int[0];
+        public Grafic()
+        {
+            Paint += Grafic_paint;
+
+            KeyDown += Grafic_KeyDown;
+        }
+
+        public int[] Valori
+        {
+            get { return valori; }
+            set
+            {
+                if(value != null)
+                {
+                    valori = value;
+                    Invalidate();
+                }
+            }
+        }
+
+        private void Grafic_paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.FillRectangle(Brushes.Red, DisplayRectangle);
+            Desenare(e.Graphics, DisplayRectangle);
+        }
+
+        private void Desenare(Graphics g, Rectangle r)
+        {
+            g.FillRectangle(Brushes.White, r);
+
+            if(valori.Length == 0)
+            {
+                return;
+            }
+
+            float W = r.Width, H = r.Height;
+            int n = valori.Length;
+            float w = W / n, f = ((float)H / valori.Max());
+
+            for(var i = 0; i < n; i++)
+            {
+                float h = valori[i] * f;
+                RectangleF rElem = new RectangleF(r.Left + i * w + 0.1f *w, r.Top + H - h,
+                                    w * 0.8f, h);
+
+                g.FillRectangle(Brushes.Red, rElem);
+            }
+        }
+
+        private void Grafic_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control && e.KeyCode == Keys.P)
+            {
+                PrintDocument document = new PrintDocument();
+                document.PrintPage += (s, ea) =>
+                 {
+                     Desenare(ea.Graphics, ea.MarginBounds);
+                     ea.HasMorePages = false;                     
+                 };
+                PrintPreviewDialog ppd = new PrintPreviewDialog();
+                ppd.Document = document;
+                ppd.ShowDialog(this);
+            }
+        }
+    }
+
+  
+}
